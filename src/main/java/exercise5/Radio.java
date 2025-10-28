ab903f084a93664613b408a625a8f1735788a0ca
aragorn <zulaufa@bzz.ch>
2025-10-28 11:50:43 +0100

    Übung 5 gelöst

--- src/main/java/exercise5/Radio.java
+++ src/main/java/exercise5/Radio.java
@@ -1,5 +1,59 @@
 package exercise5;
 
 public class Radio {
+    boolean isOn;
+    int volume;
+    double frequency;
 
+    public void on(){
+        isOn = true;
+    }
+
+    public void off(){
+        isOn = false;
+    }
+
+    public void increaseVolume(){
+        volume++;
+    }
+
+    public void decreaseVolume(){
+        volume--;
+    }
+
+    public void selectChannel(double frequency){
+        this.frequency = frequency;
+    }
+
+    public String toString() {
+        if (isOn) {
+            return "The Radio is on. Volume: " + volume + " Frequency: " + frequency;
+        }else {
+            return "The Radio is off. Volume: " + volume + " Frequency: " + frequency;
+        }
+    }
+
+    public double getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(double frequency) {
+        this.frequency = frequency;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean getisOn() {
+        return isOn;
+    }
+
+    public void setisOn(boolean on) {
+        isOn = on;
+    }
 }
--- src/test/java/exercise5/RadioTest.java
+++ src/test/java/exercise5/RadioTest.java
@@ -1,7 +1,55 @@
 package exercise5;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
+    Radio radio;
+
+    @BeforeEach
+    void setUp() {
+        radio = new Radio();
+    }
+
+    @AfterEach
+    void tearDown() {
+        radio = null;
+    }
+
+    @Test
+    void on() {
+        radio.on();
+        assertTrue(radio.getisOn());
+    }
+
+    @Test
+    void off() {
+        radio.off();
+        assertFalse(radio.getisOn());
+    }
+
+    @Test
+    void increaseVolume() {
+        radio.setVolume(1);
+        radio.increaseVolume();
+        assertEquals(2, radio.getVolume());
+    }
+
+    @Test
+    void decreaseVolume() {
+        radio.setVolume(1);
+        radio.decreaseVolume();
+        assertEquals(0, radio.getVolume());
+    }
+
+    @Test
+    void selectChannel() {
+        radio.setFrequency(10123);
+        radio.selectChannel(3323);
+        assertEquals(3323, radio.getFrequency());
+    }
 }
