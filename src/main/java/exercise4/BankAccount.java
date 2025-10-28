ccc8cb36143226a8f8d68dd42f4862d27024624f
aragorn <zulaufa@bzz.ch>
2025-10-28 11:35:14 +0100

    Übung 4 fertig gelöst

--- src/main/java/exercise4/BankAccount.java
+++ src/main/java/exercise4/BankAccount.java
@@ -1,48 +1,74 @@
 package exercise4;
 
 public class BankAccount {
+    public double balance;
+    public String name;
+    public String surname;
+    int accountNumber;
+    int limit;
 
-  public double balance;
-  public String name;
-  public String surname;
-  int accountNumber;
-  int limit;
-
-  public BankAccount(String name, String surname, int accountNumber, double balance, int limit) {
+    public BankAccount(String name, String surname, int accountNumber, double balance, int limit) {
       this.name = name;
       this.surname = surname;
       this.accountNumber = accountNumber;
       this.balance = balance;
       this.limit = limit;
   }
-
+    public BankAccount(String name, String surname, double balance, int limit) {
+      this.name = name;
+      this.surname = surname;
+      this.accountNumber = (int)(Math.random() * 1000000000);
+      this.balance = balance;
+      this.limit = limit;
+  }
     public BankAccount(String name, String surname, int accountNumber, double balance) {
         this.name = name;
         this.surname = surname;
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
-
+    public BankAccount(String name, String surname, double balance) {
+        this.name = name;
+        this.surname = surname;
+        this.accountNumber = (int)(Math.random() * 1000000000);
+        this.balance = balance;
+    }
     public BankAccount(String name, String surname, int accountNumber) {
         this.name = name;
         this.surname = surname;
         this.accountNumber = accountNumber;
     }
+    public BankAccount(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+        this.accountNumber = (int)(Math.random() * 1000000000);
+    }
 
-  public void withdraw(int amount) {
-      if (limit >= amount) {
-          balance -= amount;
-          if (balance < 0) {
-              balance += amount;
-          }
-      }
-  }
+    public void withdraw(double amount) {
+        if (limit >= amount) {
+            balance -= amount;
+            if (balance < 0) {
+                balance += amount;
+                System.out.println("ERROR: Your account can't go into the negative");
+            }
+        }else {
+            System.out.println("ERROR: Your limit is " + limit);
+        }
+    }
 
-  public void deposit(double amount) {
-      balance += amount;
-  }
+    public void deposit(double amount) {
+        balance += amount;
+    }
 
-  public String toString() {
-      return "Name: " + name + " Surname: " + surname + " AccountNumber: " + accountNumber + " Balance: " + balance + " Limit: " + limit;
-  }
+    public void printBalance(){
+        System.out.println("Your balance is " + balance);
+    }
+
+    public String toString() {
+        return "Name: " + name
+                + "\nSurname: " + surname
+                + "\nAccountNumber: " + accountNumber
+                + "\nBalance: " + balance
+                + "\nLimit: " + limit;
+    }
 }
--- src/main/java/exercise4/Main.java
+++ src/main/java/exercise4/Main.java
@@ -1,7 +1,52 @@
 package exercise4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean programRunning = true;
+        char input;
+        double numberInput;
+
+        String name;
+        String surname;
+        int limit;
+        double balance;
+
+        System.out.println("What is your name?");
+        name = sc.nextLine();
+        System.out.println("What is your surname?");
+        surname = sc.nextLine();
+        System.out.println("What is your balance?");
+        balance = sc.nextDouble();
+        System.out.println("What is your limit?");
+        limit = sc.nextInt();
+        BankAccount bankAccount = new BankAccount(name, surname, balance, limit);
+
+        while (programRunning) {
+            System.out.println("Deposit (d) | Withdraw (w) | Account Details (a) | Show Balance (b) | Quit (q)");
+            input = sc.next().toLowerCase().charAt(0);
+
+            if (input == 'd'){
+                System.out.println("Hpw much do you want to deposit?");
+                numberInput = sc.nextDouble();
+                bankAccount.deposit(numberInput);
+            }else if (input == 'w'){
+                System.out.println("Hpw much do you want to withdraw?");
+                numberInput = sc.nextDouble();
+                bankAccount.withdraw(numberInput);
+            }else  if (input == 'a'){
+                System.out.println(bankAccount);
+            }else  if (input == 'b'){
+                bankAccount.printBalance();
+            }else if (input == 'q'){
+                programRunning = false;
+            }else {
+                System.out.println("Invalid input");
+            }
+        }
 
     }
 }
